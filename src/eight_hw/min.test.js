e6fef19f78d784192705218f4dc9739916ca1361
Aleksandra Nevskaia <sashafomina3@gmail.com>
2022-09-18 14:47:35 +0300

    solved number eight

--- src/eight_hw/min.test.js
+++ src/eight_hw/min.test.js
@@ -2,6 +2,9 @@ import { getMinutesToday } from "./min";
 
 describe("minutes passed", () => {
   it("returns number of minutes", () => {
-    expect(getMinutesToday()).toEqual(796);
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date("2017-11-25T12:00:56z"));
+    const date = new Date();
+    expect(getMinutesToday(date)).toEqual(900);
   });
 });
