4d1783d777111303242c51ced906abd9bff034f4
Aleksandra Nevskaia <sashafomina3@gmail.com>
2022-09-18 12:57:35 +0100

    fixed clock

--- src/eight_hw/min.test.js
+++ src/eight_hw/min.test.js
@@ -5,6 +5,6 @@ describe("minutes passed", () => {
     jest.useFakeTimers();
     jest.setSystemTime(new Date("2022-01-01"));
     const date = new Date();
-    expect(getMinutesToday(date)).toEqual(180);
+    expect(getMinutesToday(date)).toEqual(0);
   });
 });
