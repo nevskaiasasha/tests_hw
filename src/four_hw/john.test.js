5aa0e8133c4aa366b1fa377e0ce3966420b938ad
Aleksandra Nevskaia <sashafomina3@gmail.com>
2022-09-30 22:18:14 +0100

    solved fourth

--- src/four_hw/john.test.js
+++ src/four_hw/john.test.js
@@ -4,7 +4,7 @@ jest.spyOn(window, "prompt").mockReturnValue(12);
 
 describe("objects", () => {
   const adm = new User("admin");
-  it("should have a name", () => {
+  it("should have a given name", () => {
     expect(adm.name).toContain("admin");
   });
   it("callFn alerts value entered to prompt", () => {
